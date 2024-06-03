3c9ce19b56e3d9043bc651b827a07fdac450470d
Youssef <youssefbenhamidaaa@gmail.com>
2024-06-03 11:40:52 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,45 @@
-import logo from './logo.svg';
 import './App.css';
+import React from 'react';
+import Timer from './Timer';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+class App extends React.Component {
+
+  constructor(){
+    super()
+    this.state = {
+      person : 
+      {fullName : "Mariem",
+      Bio : "J'aime la nature et la mode ",
+      imgSrc :"https://th.bing.com/th/id/R.b0a97b43a3f11a974156241ece81f316?rik=fBfGMS%2bLvF0G2w&pid=ImgRaw&r=0" ,
+      profession:"Ingénieur méchanique"
+    },
+    Show:false,
+  }}
+
+handelShow =()=>{
+  this.setState({Show : ! this.state.Show}) 
 }
 
-export default App;
+
+
+  render()
+  {
+    return (
+      <>
+
+      <button onClick={this.handelShow}>{this.state.Show ? "Hide person" : "Show person"}</button> 
+      <div className='Profil'>
+      <h4>{this.state.Show && this.state.person.fullName}</h4>
+      <h4>{ this.state.Show && this.state.person.Bio}</h4>
+      <img src={this.state.Show && this.state.person.imgSrc} />
+      </div>
+      {this.state.Show && <Timer/> } 
+
+      </>
+    )
+  }
+
+}
+export default App
+
